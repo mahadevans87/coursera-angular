12790afd4c34481cb4c2931df99316b2ea19ee3b
Mahadevan Sreenivasan <mahadevan_sreenivasan@trimble.com>
2017-01-13 16:47:42 +0530

    * Initial version of module-1 submission

---
+++ module-1/js/app.js
@@ -0,0 +1,31 @@
+(function () {
+'use strict';
+
+angular.module("LunchChecker", []).controller ("TooMuchFoodController", TooMuchFoodController);
+
+TooMuchFoodController.$inject = ["$scope", "$filter"]; 
+function TooMuchFoodController ($scope, $filter) {
+    $scope.lunchItems = "";
+    $scope.message = "";
+    
+    $scope.checkAppetite = function () {
+        var count = 0;
+        var items = $scope.lunchItems.split(",");
+        for (var item in items) {
+            if (items[item].trim() != '') {
+                count++;
+            }
+        }
+        if (count == 0) {
+            $scope.message = "Please enter data first"
+        }
+        else if (count < 4) {
+            $scope.message = "Enjoy!";
+        } else {
+            $scope.message = "Too much!";
+        }
+    }
+    
+    
+}
+})();
